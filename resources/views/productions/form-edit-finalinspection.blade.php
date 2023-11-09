1bd0db674c65dca3053ec58a6a1883ab78af282f
NattapongLam <it.9nat@gmail.com>
2023-11-09 10:28:15 +0700

    ok

--- resources/views/productions/form-edit-finalinspection.blade.php
+++ resources/views/productions/form-edit-finalinspection.blade.php
@@ -82,19 +82,19 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 col-md-3">
+                <div class="col-12 col-md-2">
                     <div class="form-group">
                         <label for="finalInspection_hd_shipto">Ship to</label>
                         <input type="text" class="form-control" value="{{$hd->finalInspection_hd_shipto}}" readonly>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
+                <div class="col-12 col-md-2">
                     <div class="form-group">
                         <label for="finalInspection_hd_dimensions">Shipping dimensions (cm.)</label>
                         <input type="text" class="form-control" value="{{$hd->finalInspection_hd_dimensions}}" readonly>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
+                <div class="col-12 col-md-2">
                     <div class="form-group">
                         <label for="finalInspection_hd_weight">Weight (kg.)</label>
                         <input type="text" class="form-control" value="{{$hd->finalInspection_hd_weight}}" readonly>
@@ -168,20 +168,20 @@
                     <div class="card card-primary card-outline card-outline-tabs">
                       <div class="card-header p-0 border-bottom-0">
                         <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
-                          <li class="nav-item">
+                          {{-- <li class="nav-item">
                             <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="true">1.การตรวจสอบในกระบวนการผลิต</a>
                           </li>
                           <li class="nav-item">
                             <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill" href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">2.การตรวจสอบขั้นสุดท้าย</a>
-                          </li>
+                          </li> --}}
                           <li class="nav-item">
-                            <a class="nav-link" id="custom-tabs-four-profile1-tab" data-toggle="pill" href="#custom-tabs-four-profile1" role="tab" aria-controls="custom-tabs-four-profile1" aria-selected="false">เอกสารแนบ</a>
+                            <a class="nav-link active" id="custom-tabs-four-profile1-tab" data-toggle="pill" href="#custom-tabs-four-profile1" role="tab" aria-controls="custom-tabs-four-profile1" aria-selected="false">เอกสารแนบ</a>
                           </li>
                         </ul>
                       </div>
                       <div class="card-body">
                         <div class="tab-content" id="custom-tabs-four-tabContent">
-                          <div class="tab-pane fade show active" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="custom-tabs-four-home-tab">
+                          {{-- <div class="tab-pane fade show active" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="custom-tabs-four-home-tab">
                             <div class="table-responsive">
                                 <table class="table table-bordered">
                                     <thead>
@@ -204,8 +204,8 @@
                                     </tbody>
                                 </table>
                                 </div>
-                          </div>
-                          <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel" aria-labelledby="custom-tabs-four-profile-tab">
+                          </div> --}}
+                          {{-- <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel" aria-labelledby="custom-tabs-four-profile-tab">
                             <div class="table-responsive">
                                 <table class="table table-bordered">
                                     <thead>
@@ -228,8 +228,8 @@
                                     </tbody>
                                 </table>
                             </div>
-                          </div>
-                          <div class="tab-pane fade" id="custom-tabs-four-profile1" role="tabpanel" aria-labelledby="custom-tabs-four-profile1-tab">
+                          </div> --}}
+                          <div class="tab-pane fade show active" id="custom-tabs-four-profile1" role="tabpanel" aria-labelledby="custom-tabs-four-profile1-tab">
                             <div class="table-responsive">
                                 <table class="table table-bordered">        
                                     <thead>
