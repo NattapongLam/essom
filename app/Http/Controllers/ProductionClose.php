71e6855341166c1944eceeec182f8873a0de3d65
NattapongLam <it.9nat@gmail.com>
2023-11-13 13:50:20 +0700

    นา

--- app/Http/Controllers/ProductionClose.php
+++ app/Http/Controllers/ProductionClose.php
@@ -128,6 +128,22 @@ class ProductionClose extends Controller
                     ]);
                 }
                 DB::commit();
+                $sta = DB::table('productionopenjob_status')->where('productionopenjob_status_id',$request->productionopenjob_status_id)->first();
+                define('LINE_API', "https://notify-api.line.me/api/notify");
+                $token = "bz5HNGdmNUwOZ4z44oxTsoi1iJ74RJqPmvyHAfTX3SS";
+                $params = array(
+                "message"  => "แจ้งเตือนเอกสารปิดงาน"."\n"
+                ."วันที่เริ่ม - จบ : ".date("d-m-Y",strtotime($hd->productionopenjob_hd_startdate))." - ".date("d-m-Y",strtotime($hd->productionopenjob_hd_enddate))."\n"
+                ."เลขที่ : ".$hd->productionopenjob_hd_docuno."\n"
+                ."ลูกค้า : ".str_replace(' ','',$hd->ms_customer_name)."\n"
+                ."สินค้า : ".$hd->ms_product_name."\n"
+                ."Spec Page : ".$hd->ms_specpage_name."\n"
+                ."ประมาณการต้นทุน : ".$hd->productionopenjob_actualcost."\n"
+                ."ผู้อนุมัติ : ".Auth::user()->name." สถานะ :" . $sta->productionopenjob_status_name. "\n",
+                "stickerPkg"     => 446,
+                "stickerId"      => 1988,
+                );
+                $res = $this->notify_message($params, $token);
                 return redirect()->route('pd-close.index')->with('success', 'บันทึกข้อมูลสำเร็จ');
             }catch(\Exception $e){
                 Log::error($e->getMessage());
@@ -152,6 +168,22 @@ class ProductionClose extends Controller
                     ]);
                 }
                 DB::commit();
+                $sta = DB::table('productionopenjob_status')->where('productionopenjob_status_id',$request->productionopenjob_status_id)->first();
+                define('LINE_API', "https://notify-api.line.me/api/notify");
+                $token = "bz5HNGdmNUwOZ4z44oxTsoi1iJ74RJqPmvyHAfTX3SS";
+                $params = array(
+                "message"  => "แจ้งเตือนเอกสารปิดงาน"."\n"
+                ."วันที่เริ่ม - จบ : ".date("d-m-Y",strtotime($hd->productionopenjob_hd_startdate))." - ".date("d-m-Y",strtotime($hd->productionopenjob_hd_enddate))."\n"
+                ."เลขที่ : ".$hd->productionopenjob_hd_docuno."\n"
+                ."ลูกค้า : ".str_replace(' ','',$hd->ms_customer_name)."\n"
+                ."สินค้า : ".$hd->ms_product_name."\n"
+                ."Spec Page : ".$hd->ms_specpage_name."\n"
+                ."ประมาณการต้นทุน : ".$hd->productionopenjob_actualcost."\n"
+                ."ผู้อนุมัติ : ".Auth::user()->name." สถานะ :" . $sta->productionopenjob_status_name. "\n",
+                "stickerPkg"     => 446,
+                "stickerId"      => 1988,
+                );
+                $res = $this->notify_message($params, $token);
                 return redirect()->route('pd-close.index')->with('success', 'บันทึกข้อมูลสำเร็จ');
             }catch(\Exception $e){
                 Log::error($e->getMessage());
@@ -185,5 +217,27 @@ class ProductionClose extends Controller
                 'dt' => $dt,
 
             ]);
+    }
+    function notify_message($params, $token)
+    {
+        $queryData = array(
+            'message'          => $params["message"],
+            'stickerPackageId' => $params["stickerPkg"],
+            'stickerId'        => $params["stickerId"],
+        );
+        $queryData = http_build_query($queryData, '', '&');
+        $headerOptions = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
+                    . "Authorization: Bearer " . $token . "\r\n"
+                    . "Content-Length: " . strlen($queryData) . "\r\n",
+                'content' => $queryData,
+            ),
+        );
+        $context = stream_context_create($headerOptions);
+        $result = file_get_contents(LINE_API, FALSE, $context);
+        $res = json_decode($result);
+        return $res;
     }
 }
