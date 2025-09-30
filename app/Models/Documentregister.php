01debce46a25b68667a1d9796a2d97a6a94bd952
NattapongLam <92558627+NattapongLam@users.noreply.github.com>
2025-09-30 11:39:04 +0700

    ok

--- app/Models/Documentregister.php
+++ app/Models/Documentregister.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Documentregister extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'documentregisters_id';
+    protected $table = 'documentregisters';
+    protected $guarded = ['documentregisters_id'];
 }
--- database/migrations/2025_09_29_114407_create_documentregisters_table.php
+++ database/migrations/2025_09_29_114407_create_documentregisters_table.php
@@ -14,7 +14,23 @@ class CreateDocumentregistersTable extends Migration
     public function up()
     {
         Schema::create('documentregisters', function (Blueprint $table) {
-            $table->id();
+            $table->id('documentregisters_id');
+            $table->integer('documentregisters_listno');
+            $table->string('documentregisters_docuno');
+            $table->string('documentregisters_remark')->nullable();
+            $table->string('documentregisters_rev01')->nullable();
+            $table->string('documentregisters_rev02')->nullable();
+            $table->string('documentregisters_rev03')->nullable();
+            $table->string('documentregisters_rev04')->nullable();
+            $table->string('documentregisters_rev05')->nullable();
+            $table->string('documentregisters_rev06')->nullable();
+            $table->string('documentregisters_rev07')->nullable();
+            $table->string('documentregisters_rev08')->nullable();
+            $table->string('documentregisters_rev09')->nullable();
+            $table->string('documentregisters_rev10')->nullable();
+            $table->string('documentregisters_file')->nullable();
+            $table->string('person_at');
+            $table->boolean('documentregisters_flag')->default(true); 
             $table->timestamps();
         });
     }
