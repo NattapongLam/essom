75e7cc16127467f6d1bd9fb341461ec3151f6019
NattapongLam <92558627+NattapongLam@users.noreply.github.com>
2025-11-11 14:29:13 +0700

    ok

--- app/Models/Designplan.php
+++ app/Models/Designplan.php
@@ -39,13 +39,5 @@ class DesignPlan extends Model
         'planned_marketing','planned_date_marketing',
         'planned_plant','planned_date_plant',
         'approved_by','approved_date',
-        'reviewed_status',
-        'approved_status_request',
-        'engineer_desing_status',
-        'senior_engineer_status',
-        'planned_status',
-        'planned_marketing_status',
-        'planned_plant_status',
-        'approved_status'
     ];
 }
