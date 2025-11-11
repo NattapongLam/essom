8f3b807451e6f7508d343c9ea5ad3651065c33d6
NattapongLam <92558627+NattapongLam@users.noreply.github.com>
2025-11-11 14:22:28 +0700

    ok

--- app/Models/Designplan.php
+++ app/Models/Designplan.php
@@ -23,21 +23,14 @@ class DesignPlan extends Model
         'reason_other',
         'design_input_1','design_input_2','design_input_3','design_input_4',
         'design_input_5','design_input_6','design_input_7','design_input_8',
-
- 
         'ref_brand1','ref_model1','ref_brand2','ref_model2',
-
         'requested_by','requested_date',
         'reviewed_by','reviewed_date',
         'approved_by_request','approved_date_request', 
-
-      
         'engineer_desing', 'senior_engineer',
         'plan_calc','act_calc','plan_review','act_review','participants',
         'plan_verify','act_verify','plan_proto','act_proto',
-        'plan_valid','act_valid','plan_final','act_final',
-
-       
+        'plan_valid','act_valid','plan_final','act_final',       
         'planned_by','planned_date_engineering',
         'planned_marketing','planned_date_marketing',
         'planned_plant','planned_date_plant',
